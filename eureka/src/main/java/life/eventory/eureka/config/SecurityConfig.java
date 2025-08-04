1005a69939cc4bfd3a76d03c020130e7fc4131e1
donggyun-han <hdg5639@daum.net>
2025-08-04 12:10:36 +0900

    refactor: eureka 패키지 그룹 변경 - com.example -> life.eventory

--- eureka/src/main/java/life/eventory/eureka/config/SecurityConfig.java
+++ eureka/src/main/java/life/eventory/eureka/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.eureka.config;
+package life.eventory.eureka.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
