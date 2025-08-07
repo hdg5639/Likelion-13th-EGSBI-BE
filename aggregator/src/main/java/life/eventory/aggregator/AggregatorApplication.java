1960c1a2cf36771a7f728b438a480d565669254f
donggyun-han <hdg5639@daum.net>
2025-08-07 12:05:42 +0900

    chore: Swagger Aggregator 생성 - Swagger Aggregator 생성

---
+++ aggregator/src/main/java/life/eventory/aggregator/AggregatorApplication.java
@@ -0,0 +1,15 @@
+package life.eventory.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregatorApplication.class, args);
+    }
+
+}
