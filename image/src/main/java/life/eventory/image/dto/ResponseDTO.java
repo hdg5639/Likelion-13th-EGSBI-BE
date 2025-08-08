0b13f1cfcae217724b8ce788385de08d55d1fb96
donggyun-han <hdg5639@daum.net>
2025-08-08 16:20:45 +0900

    docs: image 스웨거 문서 작성 - 컨트롤러 스웨거 문서 작성 - 응답 DTO에 스키마 추가

--- image/src/main/java/life/eventory/image/dto/ResponseDTO.java
+++ image/src/main/java/life/eventory/image/dto/ResponseDTO.java
@@ -1,5 +1,6 @@
 package life.eventory.image.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Builder
@@ -7,7 +8,10 @@ import lombok.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "이미지 업로드 후 반환되는 정보")
 public class ResponseDTO {
+    @Schema(description = "이미지 고유 ID", example = "123")
     private Long id;
+    @Schema(description = "저장된 파일 이름 (UUID 기반)", example = "f1f2443a-9e41-4a92-ae6c-bb74d5b5a5c9.jpg")
     private String storedFileName;
 }
