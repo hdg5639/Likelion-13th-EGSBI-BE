e3c53c8cdc8036255c650e7989b6658c9a7ed3df
donggyun-han <hdg5639@daum.net>
2025-08-12 15:04:20 +0900

    fix: gateway cors 수정 - 중복 헤더 제거

--- gateway/src/main/java/life/eventory/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/life/eventory/gateway/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package life.eventory.gateway.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.factory.DedupeResponseHeaderGatewayFilterFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -79,5 +82,15 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
+    }
+
+    @Bean
+    public GlobalFilter dedupeHeaders() {
+        GatewayFilter gf = new DedupeResponseHeaderGatewayFilterFactory()
+                .apply(c -> {
+                    c.setName("Access-Control-Allow-Origin");
+                    c.setStrategy(DedupeResponseHeaderGatewayFilterFactory.Strategy.RETAIN_UNIQUE);
+                });
+        return gf::filter;
     }
 }
