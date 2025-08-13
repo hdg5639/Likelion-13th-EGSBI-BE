9f5aa5f3d067fd7839e0e1fd8d176a6a4b604268
eunseo16 <dmstj1116@naver.com>
2025-08-14 03:26:19 +0900

    fix: findByHistory 구문 파싱 실패 오류 수정 - 쿼리 작성

--- activity/src/main/java/life/eventory/activity/repository/HistoryRepository.java
+++ activity/src/main/java/life/eventory/activity/repository/HistoryRepository.java
@@ -4,6 +4,8 @@ import life.eventory.activity.entity.HistoryEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -12,5 +14,7 @@ import java.util.Optional;
 @Repository
 public interface HistoryRepository extends JpaRepository<HistoryEntity, Long> {
     Page<HistoryEntity> findByUserId(Long userId, Pageable pageable);
-    Optional<HistoryEntity> findByHistory(Long userId, Long eventId, LocalDateTime time);
+
+    @Query("SELECT h FROM HistoryEntity h WHERE h.userId = :userId AND h.eventId = :eventId AND h.viewedAt >= :time")
+    Optional<HistoryEntity> findByHistory(@Param("userId") Long userId, @Param("eventId") Long eventId,@Param("time") LocalDateTime time);
 }
