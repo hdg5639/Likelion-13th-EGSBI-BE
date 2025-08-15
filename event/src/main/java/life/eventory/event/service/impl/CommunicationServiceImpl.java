3489d4a66ebe42a6119cd848f1615bef2eead913
donggyun-han <hdg5639@daum.net>
2025-08-15 17:11:05 +0900

    refactor: event 행사 생성 유저 확인 로직 추가 - 주최자 존재 여부 확인 로직 추가 - User 요청 코드 추가

--- event/src/main/java/life/eventory/event/service/impl/CommunicationServiceImpl.java
+++ event/src/main/java/life/eventory/event/service/impl/CommunicationServiceImpl.java
@@ -47,12 +47,6 @@ public class CommunicationServiceImpl implements CommunicationService {
 //                .build()
 //                .toUri();
 
-        // RestTemplate 검사
-        if (restTemplate == null) {
-            log.error("RestTemplate is null");
-            throw new IllegalStateException("RestTemplate is not initialized");
-        }
-
         // 요청 헤더 생성
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
@@ -87,12 +81,6 @@ public class CommunicationServiceImpl implements CommunicationService {
                 .path("/api/image/" + posterId)
                 .build()
                 .toUri();
-
-        // RestTemplate 검사
-        if (restTemplate == null) {
-            log.error("RestTemplate is null");
-            throw new IllegalStateException("RestTemplate is not initialized");
-        }
 
         // 요청 헤더 생성
         HttpHeaders headers = new HttpHeaders();
@@ -138,6 +126,58 @@ public class CommunicationServiceImpl implements CommunicationService {
         if (instances == null || instances.isEmpty()) {
             log.error("No Image-Server instances available");
             throw new IllegalStateException("No Image-Server instances available");
+        }
+
+        return instances.get(0);
+    }
+
+    @Override
+    public void existUser(Long userId) {
+        ServiceInstance imageInstance = getUserInstance();
+
+        // 요청 url 생성
+        URI uri = UriComponentsBuilder.fromUri(imageInstance.getUri())
+                .path("/api/user/exist/" + userId)
+                .build()
+                .toUri();
+
+        // 요청 헤더 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // 요청 HTTP 엔티티 생성
+        HttpEntity<Boolean> requestEntity = new HttpEntity<>(null, headers);
+
+        try {
+            ResponseEntity<Boolean> response =
+                    restTemplate.exchange(uri,
+                            HttpMethod.GET,
+                            requestEntity,
+                            Boolean.class);
+
+            if (response.getStatusCode().is2xxSuccessful() &&
+                    Objects.requireNonNull(response.getBody())) {
+                return;
+            }
+
+            throw new IllegalStateException("Failed to check exist of user");
+        } catch (Exception e) {
+            imageErrorLog(e);
+            throw new IllegalStateException("Failed to send request to User-Server", e);
+        }
+    }
+
+    // User 인스턴스 조회
+    private ServiceInstance getUserInstance() {
+        if (discoveryClient == null) {
+            throw new IllegalStateException("discoveryClient is null");
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("USER");
+        log.info("Found {} USER-SERVER instances", instances != null ? instances.size() : 0);
+
+        if (instances == null || instances.isEmpty()) {
+            log.error("No User-Server instances available");
+            throw new IllegalStateException("No User-Server instances available");
         }
 
         return instances.get(0);
